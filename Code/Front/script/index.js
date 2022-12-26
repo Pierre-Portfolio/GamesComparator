b7b1a645471dbd840f77871453a34df7671759c5
Trynjitsu <60719810+Pierre-Portfolio@users.noreply.github.com>
2022-12-26 22:52:22 +0100

    update front

---
+++ Code/Front/script/index.js
@@ -0,0 +1,18 @@
+// On ajoute un listener pour savoir quand l'utilisateur appuie sur la touche entrer
+document.addEventListener("keypress", function(e) {
+  if (e.key === "Enter") {
+    console.log("New item created!");
+
+    // Récupère tous les éléments de la page
+    var elements = document.querySelectorAll("*");
+
+    // Parcoure chaque élément
+    elements.forEach(function(element) {
+      // Vérifie si l'élément possède la classe "hidden"
+      if (element.classList.contains("hidden")) {
+        // Retire la classe "hidden" de l'élément
+        element.classList.remove("hidden");
+      }
+    });
+  }
+});
