fa0b05adea128f631f1b9c23b8eb5b365f7955e6
Trynjitsu <60719810+Pierre-Portfolio@users.noreply.github.com>
2023-01-12 23:54:19 +0100

    update mvc

--- siteweb/siteweb/settings.py
+++ siteweb/siteweb/settings.py
@@ -54,7 +54,7 @@ ROOT_URLCONF = 'siteweb.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ["siteweb/templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
