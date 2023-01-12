f9795f09da6e1babdf8072502f5ed563b6ac8805
Trynjitsu <60719810+Pierre-Portfolio@users.noreply.github.com>
2023-01-13 00:58:35 +0100

    update L

--- siteweb/siteweb/urls.py
+++ siteweb/siteweb/urls.py
@@ -17,8 +17,10 @@ from django.contrib import admin
 from django.urls import path
 
 from .views import *
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('home/', homepage_render),
-]
+    path('home/',homepage_render),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
