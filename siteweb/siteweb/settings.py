89d5edb484e0c10ce60e84779bfe091249fdeb23
Trynjitsu <60719810+Pierre-Portfolio@users.noreply.github.com>
2023-01-13 01:57:54 +0100

    uopdate mvc django

--- siteweb/siteweb/settings.py
+++ siteweb/siteweb/settings.py
@@ -64,6 +64,7 @@ TEMPLATES = [
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
+                'django.template.context_processors.static',
             ],
         },
     },
@@ -123,3 +124,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static/'),
+]
